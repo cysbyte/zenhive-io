0d20789e1c4c8213688f4132a8b1b6c87d4d374e
cysbyte <chenyunshu2@gmail.com>
2025-01-10 18:01:45 +0800

    feat: adjust ui

--- src/components/zenhive/Hero.tsx
+++ src/components/zenhive/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <ContentContainer>
             <div className='w-full md:w-[850px] z-50 relative mt-20 md:mt-44 h-full'>
                 <h1 className='font-poppins font-medium text-[32px] leading-[44px] md:text-[54px] md:leading-[65px] text-white -mt-20 md:-mt-0'><span className='text-[#C0EE02]'>Empowering Industries</span> with Real-Time AI, Optimized Data, and DePIN Innovation</h1>
-                <div className='hidden md:flex justify-start items-center gap-4 mt-14'>
+                {/* <div className='hidden md:flex justify-start items-center gap-4 mt-14'>
                     <button className='flex justify-center items-center gap-2 px-4 py-2.5 bg-gradient-to-r from-[#C0EE02] to-[#90FF6B] rounded-lg'>
                         <p className='font-pingFangSCRegular text-xl font-semibold'>Get Started with ZenHive</p>
                         <img src={go} alt="" />
@@ -17,7 +17,7 @@ const Hero = () => {
                         <p className='font-pingFangSCLight text-xl text-white '>Explore Applications</p>
                         <img src={go1} alt="" />
                     </button>
-                </div>
+                </div> */}
             </div>
 
         </ContentContainer>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,11 +26,12 @@ const Home = () => {
   return (
     <main className='overflow-x-hidden bg-black h-screen'>
       <Container>
-        <div className='w-full relative bg-[#0F1110] overflow-hidden h-[1140px] md:h-[833px]'>
+        <div className='w-full relative bg-[#0F1110] overflow-hidden h-[840px] md:h-[833px]'>
           <div className='hidden md:block absolute left-0 bottom-0 z-10 w-[1962px] h-[480px] bg-gradient-to-r from-[#76EC37] to-[#B5F60100]'></div>
           <img className='hidden md:block absolute left-0 top-0 w-[1920px] h-[1020px] z-20 -ml-0' src={bg1} alt="" />
           <img className='hidden md:block absolute top-0 xl:-right-0 right-20 z-20 -mr-16 xl:mr-[11rem]' src={bgAzen} alt="" />
-          <img className='absolute left-0 top-0 block md:hidden z-1 object-cover w-full h-full' src={bgMobile} alt="" />
+          <img className='absolute left-0 top-0 block md:hidden z-1 object-cover w-full' src={bgMobile} alt="" />
+          <img className='absolute left-0 top-0 block md:hidden z-1 object-cover w-full' src={bgMobile} alt="" />
           <Header />
           <Hero />
         </div>
